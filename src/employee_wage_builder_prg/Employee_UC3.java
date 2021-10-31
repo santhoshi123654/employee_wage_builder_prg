70c702cfc522bc620bc109bc34774892a383535d
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-01 00:19:08 +0530

    Added the UC3 progarm

---
+++ src/employee_wage_builder_prg/Employee_UC3.java
@@ -0,0 +1,32 @@
+package employee_wage_builder_prg;
+
+public class Employee_UC3 {
+	public static void main(String[] args) {
+	    
+		int IS_PRESENT=1;
+	    int IS_PART_TIME=2;
+	    int EMP_RATE_PER_HRS=20;
+	    
+	    int empHrs=0;
+		int empWage=0;
+			double randomNumber = Math.floor(Math.random()* 10)%3;
+			
+			if(IS_PRESENT == randomNumber) {
+			System.out.println("The employee is present ");
+	     		empHrs=8;
+			}
+			else if(IS_PART_TIME == randomNumber)
+			{
+				System.out.println("The employee is present but Part Time");
+				empHrs=4;
+			}
+			else
+			{
+				System.out.println("Employee is absent ");
+			}
+			  empWage=empHrs*EMP_RATE_PER_HRS;
+			  System.out.println("Emp Wage :" +empWage);
+			  
+	     }
+
+}
