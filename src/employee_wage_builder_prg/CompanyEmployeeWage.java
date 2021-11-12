b0de61459aa3c29dfe4ea468749c5dfe21e04930
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-13 01:40:26 +0530

    Added:Company Employee Program

---
+++ src/employee_wage_builder_prg/CompanyEmployeeWage.java
@@ -0,0 +1,46 @@
+package employee_wage_builder_prg;
+
+public class CompanyEmployeeWage {
+	
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public String getCompany() {
+		return company;
+	}
+
+	public int getTotalEmpWage() {
+		return totalEmpWage;
+	}
+
+	public CompanyEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+
+	public int getEmpRatePerHour() {
+		return empRatePerHour;
+	}
+
+	public int getNumOfWorkingDays() {
+		return numOfWorkingDays;
+	}
+
+	public int getMaxHoursPerMonth() {
+		return maxHoursPerMonth;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company: " + company + " is: " + totalEmpWage;
+	}
+}
